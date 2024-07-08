1f493f2dc3ccfa9fe4d1cf242026d64c7d08d8a1
TeerathSinghRawat <73867853+TeerathSinghRawat@users.noreply.github.com>
2024-07-08 12:02:46 +0530

    Update applicationtesting1

--- application.py
+++ application.py
@@ -1,14 +1,19 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
-
+import logging
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 
 application=Flask(__name__)
 app=application
-
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('error.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(file_handler)
 ##route for a home page
 
 @app.route('/')
@@ -20,16 +25,19 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
+        try:
+            data=CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            writing_score=float(request.form.get('reading_score')))
 
-        )
+        except Exception as e:
+            app.logger.error(f"Exception occurred: {e} Error 1")
+            return str(e),500
         pred_df=data.get_data_as_frame()
         pred_df['race/ethnicity']=pred_df['race_ethnicity']
         pred_df['parental level of education']=pred_df['parental_level_of_education']
@@ -39,9 +47,13 @@ def predict_datapoint():
         pred_df.drop(columns=['race_ethnicity','parental_level_of_education','test_preparation_course','writing_score','reading_score'],inplace=True)
         print(pred_df)
 
-        predict_pipeline =PredictPipeline()
-        result=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=result[0])
+        try:
+            predict_pipeline =PredictPipeline()
+            result=predict_pipeline.predict(pred_df)
+            return render_template('home.html',results=result[0])
+        except Exception as e:
+            app.logger.error(f"Exception occurred: {e} Error 2")
+            return str(e),500
 
 if __name__=="__main__":
     app.debug=True
